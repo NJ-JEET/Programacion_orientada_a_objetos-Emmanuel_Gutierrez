5275e322efd9a3259d03c43c9c9b13d61eba1124
Emmanuel Gutiérrez <cg750431@gmail.com>
2024-02-12 00:04:39 -0600

    Se agrego el conteo de pares e impares por columna.

--- src/edu/emmanuel_gutierrez/reto3/ParImpar/process/ParesEImpares.java
+++ src/edu/emmanuel_gutierrez/reto3/ParImpar/process/ParesEImpares.java
@@ -3,17 +3,17 @@ package edu.emmanuel_gutierrez.reto3.ParImpar.process;
 import java.util.Random;
 
 /**
- * Esta clase proporciona metodos para generar numeros aleatorios,
- * contar la cantidad de pares e inpares generados y
- * imprime los numeros aleatorios generados organizados por filas y columnas intrducidas por el ususario.
+ * Esta clase proporciona métodos para generar números aleatorios,
+ * contar la cantidad de pares e impares generados e imprimir los números aleatorios generados organizados por filas y columnas.
  */
 public class ParesEImpares {
 
     /**
-     * Genera una matriz de numeros aleatorios ordenados segun lo especificado.
-     * @param filas Numero de filas en la matriz.
-     * @param columnas Numero de columnas en la matriz.
-     * @return Una matriz de numeros aleatorios.
+     * Genera una matriz de números aleatorios ordenados según lo especificado.
+     *
+     * @param filas    Número de filas en la matriz.
+     * @param columnas Número de columnas en la matriz.
+     * @return Una matriz de números aleatorios.
      */
     public static int[][] generarNumerosAleatorios(int filas, int columnas) {
         Random aleatorio = new Random();
@@ -28,14 +28,19 @@ public class ParesEImpares {
     }
 
     /**
-     * Cuenta el numero de pares e imparez generados en la matriz.
-     * @param matriz Matriz de numeros.
+     * Cuenta el número de pares e impares generados en la matriz.
+     *
+     * @param matriz Matriz de números.
      */
     public static void contarParesImpares(int[][] matriz) {
-        for (int i = 0; i < matriz.length; i++) {
+        int filas = matriz.length;
+        int columnas = matriz[0].length;
+
+        // Contar por filas
+        for (int i = 0; i < filas; i++) {
             int pares = 0;
             int impares = 0;
-            for (int j = 0; j < matriz[i].length; j++) {
+            for (int j = 0; j < columnas; j++) {
                 if (matriz[i][j] % 2 == 0) {
                     pares++;
                 } else {
@@ -44,17 +49,34 @@ public class ParesEImpares {
             }
             System.out.println("Cantidad de pares: " + pares + ", impares: " + impares + " en la fila " + (i + 1));
         }
+        System.out.println(); // Agregar un salto de línea después del conteo por filas
+
+        // Contar por columnas
+        for (int j = 0; j < columnas; j++) {
+            int pares = 0;
+            int impares = 0;
+            for (int i = 0; i < filas; i++) {
+                if (matriz[i][j] % 2 == 0) {
+                    pares++;
+                } else {
+                    impares++;
+                }
+            }
+            System.out.println("Cantidad de pares: " + pares + ", impares: " + impares + " en la columna " + (j + 1));
+        }
+        System.out.println(); // Agregar un salto de línea después del conteo por columnas
     }
 
     /**
-     * Imprime los numerdos generados aleatoriamente en filas y columnas.
-     * @param matriz Matriz de numeros.
+     * Imprime los números generados aleatoriamente en filas y columnas.
+     *
+     * @param matriz Matriz de números.
      */
     public static void imprimirNumerosAleatorios(int[][] matriz) {
         System.out.println("Numeros generados de manera aleatoria y organizados en filas y columnas:");
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                System.out.print(matriz[i][j] + "\t");
+        for (int[] ints : matriz) {
+            for (int anInt : ints) {
+                System.out.print(anInt + "\t");
             }
             System.out.println();
         }
