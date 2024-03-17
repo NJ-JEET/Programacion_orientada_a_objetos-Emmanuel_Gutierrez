9e418962c622f614a1b9e92ac00e8c75ec6cf323
Emmanuel Gutiérrez <cg750431@gmail.com>
2024-03-16 23:57:02 -0600

    Actualizar la clase InterfazCalculadora: Se modificó la clase InterfazCalculadora para que utilice los nuevos métodos de las operaciones matemáticas. Actualizar la clase Main: Se actualizó la clase Main para reflejar los cambios en la estructura de paquetes y la clase principal del programa.

---
+++ src/edu/emmanuel_gutierrez/reto7/calculadora/Main.java
@@ -0,0 +1,17 @@
+/**
+ * Clase principal que inicia la aplicación de la calculadora.
+ * Invoca el método main de la clase InterfazCalculadora para iniciar la interfaz gráfica de la calculadora.
+ */
+package edu.emmanuel_gutierrez.reto7.calculadora;
+
+import edu.emmanuel_gutierrez.reto7.calculadora.ui.InterfazCalculadora;
+
+public class Main {
+    /**
+     * Método principal que inicia la aplicación de la calculadora.
+     * Invoca el método main de la clase InterfazCalculadora para iniciar la interfaz gráfica de la calculadora.
+     */
+    public static void main(String[] args){
+        InterfazCalculadora.main(args);
+    }
+}
